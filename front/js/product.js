ba6570ac0cb48443edad2100c2dd6ca86b64f3f9
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-06-30 10:47:58 +0200

    product in onepage integration completed

--- front/js/product.js
+++ front/js/product.js
@@ -1,48 +1,47 @@
-   
+//Récupération de l'Id dans notre URL    
 const urlProduct = window.location.search;
 const urlParams = new URLSearchParams(urlProduct);
-const newUrl = urlParams.get("id"); 
-console.log(newUrl)
-    
-function productAppear() {
-fetch(`http://localhost:3000/api/products/${newUrl}`)
+const newId = urlParams.get("id"); 
 
-    //première promesse dans laquelle on demande de retourner les résultats de la requête en JSON 
+//Affichage des produits dans leur page produit
+function productAppear() {
+//Intégration de l'id par produit dans notre URL afin de les distinguer dans leur page respective
+fetch(`http://localhost:3000/api/products/${newId}`)
+
+    //les resultats de la requête sont retournés en JSON 
     .then((res) => {
         if (res.ok){
             return res.json(); 
         } 
     })
 
-    //deuxième promesse qui s'execute si la première est "true" 
+    //Affichage dynamique des éléments de la page produit
     .then((data) => {
-       let productData = data; 
-
-       if (productData === data){
-        console.log("true")
-
         document
         .querySelector(".item__img")
-        .innerHTML = `<img src="${productData.imageUrl}" alt="Photographie d'un canapé"></img>`
+        .innerHTML = `<img src="${data.imageUrl}" alt="Photographie d'un canapé"></img>`
 
         document
         .querySelector(".item__content__titlePrice")
-        .innerHTML = `<h1 id="title">${productData.name}</h1>
-        <p>Prix : <span id="price">${productData.price}</span>€</p>`
+        .innerHTML = `<h1 id="title">${data.name}</h1>
+        <p>Prix : <span id="price">${data.price}</span>€</p>`
 
         document
         .querySelector(".item__content__description")
-        .innerHTML = `<p id="description">${productData.description}</p>`
+        .innerHTML = `<p id="description">${data.description}</p>`
 
-        for (let colors of productData.colors){
+        //La boucle permet de créer les valeurs à la balise "select"
+        for (let colors of data.colors){
             document
-        .querySelector("#colors").innerHTML += `<option value="${colors}">${colors}</option>` 
+        .querySelector("#colors").innerHTML += `<option value="${colors.toLowerCase()}">${colors}</option>` 
         }
-       }
-  
+       
+       /*--Déclaration de la fonction getButton qui prend pour parametre "data", qui sera nécessaire à 
+       l'importation des données dans le localstorage --*/
+        getButton(data);
+
     })
     
-
     //Active dans le cas où notre première promesse est "false"
     .catch ((err) => {
         document
@@ -54,7 +53,101 @@ fetch(`http://localhost:3000/api/products/${newUrl}`)
     
 };
 
-
-
-//declaration de la fonction productAppear
+//Declaration de la fonction productAppear
 productAppear(); 
+
+
+
+//----------------------Gestion du panier ----------------
+
+//Ajout d'un évènement à notre bouton ajout au panier
+    function getButton(productData) {
+        const button = document.getElementById("addToCart"); 
+
+        button.addEventListener("click", () => {
+            console.log("ajout au panier"); 
+        //Récupération après "clic" des couleurs et des quantités sélectionnées 
+        const selectColors = document.getElementById("colors"); 
+        const selectQty = document.getElementById("quantity"); 
+
+        //Attribution de la couleur  et de la quantité choisie à notre object
+        const optionChoice = Object.assign({}, productData, {
+            color : selectColors.value, 
+            quantity : selectQty.value, 
+        }); 
+
+        console.log("--- optionChoice --->", optionChoice);
+
+        //Insertion des données dans un tableau à destination du localstorage 
+        let productArray = [];
+        productArray.push(optionChoice);
+        console.log("--- productArray --->", productArray);
+        //--------simplification: mettre optionChoice en parametre et supprimer productArray
+        localStorage.setItem("produit", JSON.stringify(productArray));
+    
+
+
+            //-----Seconde méthode------
+
+        // let productArray = JSON.parse(localStorage.getItem("product")); 
+        // if (productArray == null){
+        //     productArray = []
+        //     productArray.push(productData)
+        //     console.log(productArray)
+        // }
+        
+
+        
+        })
+    }
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
