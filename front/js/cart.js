e6fe4ad2852bf9279ab493c164ab46c3c45cd105
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-07-09 17:24:16 +0200

    total functions integrated & function addquantity completed

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,5 @@
 //cette fonction va nous permettre d'afficher les produits contenus dans le localstorage dans notre page panier
+
 function cartAppear() {
     let addCart = JSON.parse(localStorage.getItem("productsInCart"));
     let cart__items = document.querySelector("#cart__items"); 
@@ -16,44 +17,72 @@ function cartAppear() {
                                                         <div class="cart__item__content__settings">
                                                             <div class="cart__item__content__settings__quantity">
                                                                 <p>Qté : ${item.quantity} </p>
-                                                                <input type="number" class="itemQuantity" name="itemQuantity" min="0" max="100" value="42">
+                                                                <input type="number" data-id="${item._id}" data-color="${item.color}" class="itemQuantity" name="itemQuantity" min="1" max="100" value="1">
                                                             </div>
                                                             <div class="cart__item__content__settings__delete">
                                                                 <p class="deleteItem">Supprimer</p>
                                                             </div>
                                                         </div>
-                                                    </article>`)   
-    addQuantity();
+                                                    </article>`) 
+    totalCart(addCart);                                            
+}
+cartAppear(); 
+addQuantity();  
+
+//Définition du nombre d'articles et du coût total du panier
+function totalCart(){
+    let CartRegistered = JSON.parse(localStorage.getItem("productsInCart"));
+    let totalCartQty = 0;
+    let totalCartPrice = 0; 
+
+    //la boucle récupère les quantités et les prix de tous les articles enregistrés dans le localstorage
+    for (let i = 0; i < CartRegistered.length; i++){
+
+        totalCartQty += CartRegistered[i].quantity;
+        console.log("TCQ", totalCartQty);
+        localStorage.setItem("totalQuantity", JSON.stringify(totalCartQty));
+
+        totalCartPrice += (CartRegistered[i].quantity * CartRegistered[i].price);
+        console.log("TCP", totalCartPrice);
+        localStorage.setItem("totalPrice", JSON.stringify(totalCartPrice));
+
+    }
+    totalCartAppear(); 
 }
 
-// function totalCartAppear() {
-//     let totalCart = JSON.parse(localStorage.getItem("totalQuantity"));
-//     let totalCost = JSON.parse(localStorage.getItem("totalPrice"));
-//     let totalQtyCart = document.querySelector(".cart__price");
 
-//     if (totalCart && totalCost && totalQtyCart){
-//         totalQtyCart.innerHTML = `<p>Total (<span id="totalQuantity">${totalCart}</span> articles) : <span id="totalPrice">${totalCost}</span> €</p>`;
-//     }
-// }
+function totalCartAppear() {
+    let totalCart = JSON.parse(localStorage.getItem("totalQuantity"));
+    let totalCost = JSON.parse(localStorage.getItem("totalPrice"));
+    let totalQtyCart = document.querySelector(".cart__price");
 
-// function addQuantity(){
-//     const qtyButton = document.querySelectorAll(".itemQuantity");
-//     console.log(qtyButton);
-//     //pour chaque elements on ajoute un évènement 
-//     // qtyButton.forEach((quantity) => {
-//     //     qtyButton.addEventListener("change", (e) => {
-//     //         let cartRegistered = JSON.parse(localStorage.getItem("productInCart"));
+    if (totalCart && totalCost && totalQtyCart){
+        totalQtyCart.innerHTML = `<p>Total (<span id="totalQuantity">${totalCart}</span> articles) : <span id="totalPrice">${totalCost}</span> €</p>`;
+    }
+}
 
-//     //     })
+function addQuantity(){
+    let cartRegistered = JSON.parse(localStorage.getItem("productsInCart"));
+    const qtyButton = document.querySelectorAll('.itemQuantity');
+    qtyButton.forEach((quantity) => {
+        quantity.addEventListener("change", (e) => {
+            
+            for (item of cartRegistered){
+                console.log(qtyButton);
+                if (item._id == quantity.dataset.id && item.color == quantity.dataset.color){
+                    item.quantity = parseInt(e.target.value);
+                    localStorage.setItem("productsInCart", JSON.stringify(cartRegistered));
+                    totalCart();
+                }
+            }
+        })
+    }) 
+}
 
-//     // })
-    
-    
-   
-// }
 
-cartAppear(); 
-// totalCartAppear(); 
+
+ 
+
  
 
 
