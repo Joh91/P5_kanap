79ae9bcd175714e93236a40eb05e82a54ddd9c97
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-07-17 13:31:48 +0200

    new functions

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,11 +1,21 @@
 //Comme pour la page produit, on récupère l'order-ID pour l'afficher sur la page
-const urlOrder = window.location.search;
-console.log(urlOrder)
-const urlParams = new URLSearchParams(urlOrder);
-console.log(urlParams)
-const newOrderId = urlParams.get("orderId"); 
-console.log(newOrderId)
+function getOrderId(){
+    const urlOrder = window.location.search;
+    console.log(urlOrder)
+    const urlParams = new URLSearchParams(urlOrder);
+    console.log(urlParams)
+    const newOrderId = urlParams.get("orderId"); 
+    console.log(newOrderId)
 
-//intégration du numéro de commande 
-let orderIdIntegrated = document.querySelector("#orderId")
-.innerHTML += `<span id="orderId">${newOrderId}</span></p>` 
+    //déclaration de OrderIdDisplay qui récupère la valeur de newOrderId 
+    orderIdDisplay(newOrderId)
+};
+  
+//intégration du numéro de commande dans la page confirmation 
+function orderIdDisplay(newOrderId){
+    let orderIdIntegrated = document.querySelector("#orderId")
+    .innerHTML += `<span id="orderId">${newOrderId}</span></p>`;
+};
+
+//Déclaration de getOrderId 
+getOrderId(); 
